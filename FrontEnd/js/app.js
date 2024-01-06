d2794c2426466cf4027ae471ee9a30e8595a719a
Atef Lachhab <atef.lachhab1@gmail.com>
2024-01-06 10:53:58 -0500

    Pausing p3

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,6 +1,25 @@
-import { getWorks } from "./gallery";
+// Suppression de la galerie faite en html
+document.getElementById("portfolio").remove();
 
-// Suppression des travaux existant avec le DOM
-document.querySelector("#portfolio").remove();
+//Récupération des travaux depuis l'API
+async function getWorks() {
+  try {
+    const response = await fetch("http://localhost:5678/api/works");
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP : ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (erreur) {
+    console.error(
+      "Une erreur s'est produite lors de la récupération des données :",
+      erreur
+    );
+  }
+}
+
+for (let i = 0; i < data.length; i++) {}
 
 getWorks();
--- FrontEnd/js/gallery.js
+++
@@ -1,7 +0,0 @@
-async function getWorks() {
-  const response = await fetch("http://localhost:5678/api/works");
-  const data = await response.json();
-  console.log(data);
-}
-
-export { getWorks };
