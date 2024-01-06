a138778158416e6cc711917f7044ee07a618c89d
Atef Lachhab <atef.lachhab1@gmail.com>
2024-01-06 11:50:17 -0500

    display gallery ok

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,7 +1,14 @@
 // Suppression de la galerie faite en html
-document.getElementById("portfolio").remove();
+document.querySelector(".gallery").remove();
+// Création de la div gallery pour pouvoir y append les élements de la boucle for ensuite
+const gallery = document.createElement("div");
+gallery.className = "gallery";
+document.querySelector("#portfolio").appendChild(gallery);
 
-//Récupération des travaux depuis l'API
+// Récupération des travaux
+getWorks();
+
+//Création de la fonction de récupération des travaux depuis l'API
 async function getWorks() {
   try {
     const response = await fetch("http://localhost:5678/api/works");
@@ -11,7 +18,18 @@ async function getWorks() {
     }
 
     const data = await response.json();
-    console.log(data);
+
+    // Construction de la boucle for dans la fonction pour ajouter les données de data au DOM.
+    for (let i = 0; i < data.length; i++) {
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
+      img.src = data[i].imageUrl;
+      figcaption.innerText = data[i].title;
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+      gallery.appendChild(figure);
+    }
   } catch (erreur) {
     console.error(
       "Une erreur s'est produite lors de la récupération des données :",
@@ -19,7 +37,3 @@ async function getWorks() {
     );
   }
 }
-
-for (let i = 0; i < data.length; i++) {}
-
-getWorks();
